89c7c67e70402168a54247e85db6683f90b4a233
miwa <61976102+miwa223@users.noreply.github.com>
2023-06-23 10:50:38 -0700

    add files

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,32 @@
 module.exports = {
   content: ["*"],
   theme: {
-    extend: {},
+    extend: {
+      flex: {
+        '2': '2 2 0%',
+        '3': '3 3 0%',
+        '1/3': '0 1 33.333333%',
+      },
+      colors: {
+        "shop-pink": "#ffe3ff",
+        "shop-green": "#e3ffe3",
+        "shop-grey": "#eeeeee",
+        "shop-brown": "#5d4e60",
+        "shop-light-brown": "#c4adb0",
+      },
+    },
+    fontFamily: {
+      Poppins: ["Poppins, sans-serif"]
+    },
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+          lg: "1124px",
+          xl: "1124px",
+          "2xl": "1124px",
+      },
+    },
   },
   plugins: [],
 }
-
